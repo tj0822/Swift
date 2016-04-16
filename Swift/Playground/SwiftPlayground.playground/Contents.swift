29f30cdd47a3734e79a601e213ac95557a1e5595
tj0822 <tj820822@gmail.com>
2016-04-16 15:14:50 +0900

    start

---
+++ Swift/Playground/SwiftPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var numOfStudents = 10      //변수
+let pi = 3.1415             //상수
+
+//pi = 3                //변수를 바꾸려고 하면 에러남
+
+
+numOfStudents = 5
+numOfStudents = 7
+
+var age = 40
+var numOfItems:Int = 77     //명시적으로 Int로 생성
+
+//numOfItems = 65.4         //int형에 소수를 넣으면 에러남
+
+
